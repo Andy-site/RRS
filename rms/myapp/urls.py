af978c75db80b66334c5888158398a05c71753ce
piyush <piyushphuyal77@gmail.com>
2024-03-22 13:59:33 +0545

    first push

--- rms/myapp/urls.py
+++ rms/myapp/urls.py
@@ -1,16 +1,15 @@
 from django.contrib import admin
-from django.urls import path,include
-# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('index2/', views.index2, name='index2'),
-    path('customer_signup/', views.customer_signup, name='customer_signup'),
-    path('customer_login/', views.customer_login, name='customer_login'),
+    path('cp/', views.cp, name='cp'),
     path('admin_page/', views.admin_page, name='admin_page'),
     path('basic/', views.basic, name='basic'),
+    path('handle1/', views.handle1, name='handle1'),
 
 ]
 
-# urlpatterns += staticfiles_urlpatterns()
+
--- rms/myapp/views.py
+++ rms/myapp/views.py
@@ -1,5 +1,8 @@
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import  User
 
 
 def index(request):
@@ -14,12 +17,8 @@ def basic(request):
     return render(request, "myapp/basic.html", {})
 
 
-def customer_signup(request):
-    return render(request, "myapp/customer_signup.html", {})
-
-
-def customer_login(request):
-    return render(request, "myapp/customer_login.html", {})
+def cp(request):
+    return render(request, "myapp/cust_page.html", {})
 
 
 def admin_page(request):
@@ -35,3 +34,32 @@ def admin_page(request):
     else:
         # If it's not a POST request, just render the admin login page
         return render(request, 'myapp/index2.html')
+
+
+def handle1(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        if len(username) > 10:
+            messages.error(request, "Username must be under 10 characters")
+            return redirect("cp")
+        if not username.isalnum():
+            messages.error(request, "Username must be alphanumeric!")
+            return redirect("cp")
+        if pass1 != pass2:
+            messages.error(request, "Password do not match")
+            return redirect("cp")
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.save()
+        messages.success(request, "Your account has been successfully created!")
+        return redirect("cp")
+    else:
+        return HttpResponse('404 - Not Found')
