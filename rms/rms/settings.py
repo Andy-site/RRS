9d728347b7698260423433a2a6319ad518d88883
piyush <piyushphuyal77@gmail.com>
2024-04-01 15:02:51 +0545

    Trying to merge django backend work from this branch to the master1

--- rms/rms/settings.py
+++ rms/rms/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'rms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rms',
+        'USER': 'postgres',
+        'PASSWORD': 'qqqq',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
